5653cc29d3a9053f37d0c2481b848637d30a2faa
A. Scott Tribble <scott.tribble@As-MacBook-Pro.local>
2018-01-08 12:42:33 -0800

    finished first query

--- abraham-scott-mongo/drills.js
+++ abraham-scott-mongo/drills.js
@@ -1,4 +1,24 @@
 'use strict';
 
-use tempTestDB;
+use tempTestDb;
+db.restaurants.count();
 
+// db.restaurants.find(
+//   {
+//   cuisine: {$ne: "Not Listed/Not Applicable"},
+//   borough: {$ne: "Missing"},
+//   $where: "this.grades.length > 0",
+//   grades: { $ne: "Not Yet Graded"},
+//   "grades.score": {$gte: 0}
+//   });
+
+// db.restaurants.count({ cuisine: { $ne: "Not Listed/Not Applicable"}});
+
+// db.restaurants.count({ borough: { $ne: "Missing" }});
+// db.restaurants.count({ $where: "this.grades.length === 0" });
+// db.restaurants.count({ grades: { $ne: "Not Yet Graded" }});
+// db.restaurants.count({ "grades.score": {$exists: false}});
+// db.restaurants.count({grades: {$exists: true}});
+// db.restaurants.count({ $where: "this.grades.length >= 0" });  
+  
+  
