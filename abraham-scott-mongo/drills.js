0fd4d10213c4777c8c1c40b75203718858bfc275
A. Scott Tribble <scott.tribble@As-MacBook-Pro.local>
2018-01-08 14:36:11 -0800

    finished but frustrated

--- abraham-scott-mongo/drills.js
+++ abraham-scott-mongo/drills.js
@@ -1,6 +1,7 @@
 'use strict';
 
 use tempTestDb;
+
 db.restaurants.count();
 
 // db.restaurants.find(
@@ -20,5 +21,46 @@ db.restaurants.count();
 // db.restaurants.count({ "grades.score": {$exists: false}});
 // db.restaurants.count({grades: {$exists: true}});
 // db.restaurants.count({ $where: "this.grades.length >= 0" });  
+
+
+// Create a list of all of the cuisines offered by the restaurants, with no duplicates
+// db.restaurants.distinct("cuisine").sort();
+
+// Find the first five restaurants with the lowest positive score in one of their gradings.
+
+// db.restaurants.find({
+//   $sort: {"grades.score": {$gt: 0}}
+// }).limit(5);
   
   
+// sort restaurants by the minimum score they have ever received
+
+// db.restaurants.findOne({}, {
+//   name: 1,
+//   borough: 1,
+//   grades: {
+//     $sort: {"grades.score": 1}} 
+// });
+
+// db.restaurants.find({
+//   "grades.grade":[{$sort: { "grades.grade": -1 }}]
+// });
+
+db.restaurants.find({
+  grades: { $ne: [] }, 
+  "grades.score": { $gt: 0 }
+ },
+{name:1,
+"grades.score": 1
+}).
+ sort({ "grades.score": 1 }).
+ limit(5).
+ pretty();
+
+
+// Find the restaurants which have only been graded A.
+
+// db.restaurants.find({
+//  "grades.grade": {$nin: ["B", "C", "D", "F", null]}
+// }).pretty();
+
